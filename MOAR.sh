e99149e536c303670da2a5c85d122a97ef59d2e7
Corentin Molitor <corentin.molitor@gmail.com>
2018-06-12 15:54:44 +0100

    Added usage to readme, changed options and checks, need testing...

--- MOAR.sh
+++ MOAR.sh
@@ -1,92 +1,130 @@
 #!/bin/bash
 
+# To add : 
+#	options for nucmer
+
 threads="10"
 assembly=""
-prefix="Mummer_"
+prefix="MOAR_"
 references=""
-outputDir=""
-
+mummerPath=""
+outputDir="./"
 
 function usage
 {
-	echo "This script will align an assembly against one or more reference using Mummer"
-        echo "USAGE script_mummer.sh -t [NBTHREADS] -p [PREFIX] -o [OUTPUTFOLDER]"
-        echo "-h print this help message"
+	echo "This script will align an assembly against one or more reference using Mummer:"
+	echo ""
+    echo "USAGE MOAR.sh -r [FASTA_LIST] -t [NBTHREADS] -p [PREFIX] -o [OUTPUTFOLDER]"
+    echo "-h print this help message"
 	echo "-r : a text file containing the list of fasta files to use as a reference (Required)"
-        echo "-t : number of threads to use (default: 10)"
-        echo "-a : assembly to align to the reference (Required)"
-        echo "-p : output prefix (default: Mummer_)"
-        echo "-o : output directory (default: ./)"
+    echo "-t : number of threads to use (default: 10)"
+    echo "-a : assembly to align to the reference (Required)"
+    echo "-p : output prefix (default: Mummer_)"
+	echo "-m : path to Mummer (required)"
+    echo "-o : output directory (default: ./)"
+	echo ""
 }
 
 
-while getopts r:t:a:p:o:h opt; do
-        case ${opt} in
-            h)
-				usage
-                exit 1
-            ;;
-			r)
-				references=${OPTARG}
-			;;
-            t)
-                threads=${OPTARG}
-            ;;
-			a)
-				assembly=${OPTARG}
-			;;
-			p)
-				prefix=${OPTARG}
-			;;
-			o)
-				outputDir=${OPTARG}
-			;;
-			\?)
-				echo "Invalid option: -${OPTARG}"
-				echo ""
-				usage
-				exit 1
-			;;
-			:)
-				echo "Option -${OPTARG} requires an argument"
-				echo ""
-				usage
-				exit 1
-			;;
-        esac
+while getopts r:t:a:p:m:o:h opt; do
+    case ${opt} in
+        h)
+			usage
+            exit 1
+        ;;
+		r)
+			references=${OPTARG}
+		;;
+        t)
+            threads=${OPTARG}
+        ;;
+		a)
+			assembly=${OPTARG}
+		;;
+		p)
+			prefix=${OPTARG}
+		;;
+		m)
+			mummerPath=${OPTARG}
+		;;
+		o)
+			outputDir=${OPTARG}
+		;;
+		\?)
+			echo "Invalid option: -${OPTARG}"
+			echo ""
+			usage
+			exit 1
+		;;
+		:)
+			echo "Option -${OPTARG} requires an argument"
+			echo ""
+			usage
+			exit 1
+		;;
+    esac
 done
 
 # Check if the files are readable
-if [ ! -r ${assembly} ]; then
-	echo "cannot read ${assembly}... Abort"
+if [ ! -r "${references}" ]; then
+	echo "Cannot read ${references}... Abort"
 	echo ""
 	usage
 	exit 1
 fi
 
-if [ "${outputDir}" = "" ]; then
-	echo "You must indicates a path to the output directory"
+if [ ! -r "${assembly}" ]; then
+	echo "Cannot read ${assembly}... Abort"
+	echo ""
 	usage
 	exit 1
 fi
 
+if [ ! -d "${outputDir}" ]; then
+	echo "Cannot access the output directory: ${outputDir} ... Abort"
+	echo ""
+	usage
+	exit 1
+fi
 
+echo "MOAR on `date '+%Y-%m-%d %H:%M:%S'`" > ${outputDir}/Skipped.log
 
-# Compare against Solanum Lycopersicum
-for ref in references; do
+# Compare the assembly against each file from "references"
+for ref in $(cat ${references}); do
+
+	if [ ! -r ${ref} ]; then
+		echo "Cannot read ${ref} !!! Skipping..."
+		echo "${ref}" >> ${outputDir}/Skipped.log
+	fi
+
 	cd "${outputDir}"
-	mkdir "ch${chr}_${prefix}";
-	cd "ch${chr}_${prefix}";
-	cmd="${mummerPath}/nucmer --mum -c 500 -t 100 ${ref} ${assembly}";
-	echo "${cmd}";
+	mkdir "${prefix}_${ref}";
+	cd "${prefix}_${ref}";
+	
+	cmd="${mummerPath}/nucmer --mum -c 500 -t ${threads} ${ref} ${assembly}";
+	echo "Running ${cmd} ...";
 	eval ${cmd}
-	${mummerPath}/mummer-4.0.0beta2/delta-filter -1 out.delta > out.delta.filter;
-	${mummerPath}/tools/mummer-4.0.0beta2/mummerplot -large -layout out.delta.filter;
+	${mummerPath}/delta-filter -1 out.delta > out.delta.filter;
+	${mummerPath}/mummerplot -large -layout --terminal png --prefix ${prefix}_${ref} out.delta.filter;
+	
+	# Changing styles : reducing point size and coloring forward () and reverse ()
 	sed -i 's/set style line 1  lt 1 lw 2 pt 6 ps 1/set style line 1 lc "blue" lt 2 lw 1 pt 6 ps 0.5/g' out.gp
 	sed -i 's/set style line 2  lt 3 lw 2 pt 6 ps 1/set style line 2 lc "red" lt 2 lw 1 pt 6 ps 0.5/g' out.gp
+	
+	# Replace x11 output by pdf 
+	sed -i 's/set terminal x11 font "Courier,8"//g' out.gp
+	sed -i -e '1iset terminal pdf size 200,100\' out.gp
+	sed -i -e '2iset output \'out.pdf\'\' out.gp
+
+	# Remove plot interactivity since we plot it as a pdf 
+	head -n -8 out.gp
+	
+	# Finaly print the plot
 	gnuplot out.gp
+	
 	cd ../;
 done
 
 echo "Done !"
+echo "The list of skipped sequences (if fasta not readable) are available in ${outputDir}/Skipped.log"
 exit 0
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MOAR
 MOAR (Mummer On Assembly against a Reference)
+
+This script will align an assembly against one or more reference using Mummer
+
+USAGE script_mummer.sh -r [file with reference fasta] -t [NBTHREADS] -p [PREFIX] -o [OUTPUTFOLDER]"
+
+	-r : a text file containing the list of fasta files to use as a reference (Required)
+	-t : number of threads to use (default: 10)
+	-a : assembly to align to the reference (Required)
+	-p : output prefix (default: Mummer_)
+	-o : output directory (default: ./)
